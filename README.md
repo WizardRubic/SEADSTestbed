22d560b61bae6de72f498954e6ad6ddb49617b8a
pimeri <pimeri@ucsc.edu>
2018-03-08 14:58:01 -0800

    simple flask server setup

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # SEADSTestbed
+
+
+currently 2 test routes implemented, example curl usage as follows:
+
+
+get : curl localhost:8080/get_example/
+post: curl -X POST localhost:8080/switch_example/<SWITCH NUMBER HERE>
---
+++ server.py
@@ -0,0 +1,21 @@
+from flask import Flask, abort, request
+
+app = Flask(__name__)
+
+@app.route('/get_example/',methods=['GET'])
+def get_example():
+	return "some data\n"
+
+
+@app.route('/switch_example/<int:switch_num>',methods=['POST'])
+def switch_example(switch_num):
+	print "change state of, switch #",switch_num
+
+	# output_example = "switch #"+ switch_num
+	return "switch #" + str(switch_num) + " state changed\n"
+
+
+if __name__ == '__main__':
+	app.run(host='0.0.0.0',port=int("8080"))
+
+
